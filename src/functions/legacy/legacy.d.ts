4710b3c9c9dbbf800c8ae4e69ab419eef838ac86
Andrii Iudin <andrii.iudin@ukhsa.gov.uk>
2025-07-01 10:51:47 +0100

    fix: declaration paths for legacy modules

--- src/functions/legacy/legacy.d.ts
+++ src/functions/legacy/legacy.d.ts
@@ -1,15 +1,15 @@
-declare module '../functions/legacy/is-api-info-json-schema.js' {
+declare module 'src/functions/legacy/is-api-info-json-schema.js' {
   export default function validateApiInfo(schema: any): { message: string }[];
 }
 
-declare module '../functions/legacy/is-problem-json-schema.js' {
+declare module 'src/functions/legacy/is-problem-json-schema.js' {
   export default function validateProblemSchema(schema: any): { message: string }[];
 }
 
-declare module '../functions/legacy/is-object-schema.js' {
+declare module 'src/functions/legacy/is-object-schema.js' {
   export default function validateObjectSchema(schema: any): { message: string }[];
 }
 
-declare module '../functions/legacy/count-resource-types.js' {
+declare module 'src/functions/legacy/count-resource-types.js' {
   export default function countResourceTypes(schema: any): Record<string, number>;
 }
