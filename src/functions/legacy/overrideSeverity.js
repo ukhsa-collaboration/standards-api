c43ed312c3ae641723f8b734ae799a63dc1b88e6
Jake Aitchison <jake.aitchison@answerdigital.com>
2025-10-01 15:31:04 +0100

    refactor: experimentation

--- src/functions/legacy/overrideSeverity.js
+++ src/functions/legacy/overrideSeverity.js
@@ -1,35 +1,77 @@
 'use strict';
 
+import { createRulesetFunction } from "@stoplight/spectral-core";
+//import { JsonPath } from '@stoplight/types';
+
 /**
  * @import Core from "@stoplight/spectral-core"
  */
 
 /**
- * Overrides certain rule severities for legacy APIs based on the API type.
+ * @typedef {object} options
+ * @property {object} rulesToAdjust - A mapping of rule IDs to their new severity levels.
+ * @property {string} value - The expected value indicating a legacy API.
+ */
+
+/**
+ * Overrides certain rule severities for rules based on custom jsonpath condition.
  * @type {Core.RulesetFunction<any, null>}
  * @param {any} targetValue - The value to validate.
- * @param {null} _options - Additional options (not used).
- * @param {any} context - The context.
+ * @param {any} options - Additional options (not used).
+ * @param {Core.RulesetFunctionContext} context - The context.
  */
-export default (targetValue, _options = null, context) => {
+const overrideSeverity = (targetValue, options = null, context) => {
   // Access the current ruleset
-  const { rule } = (context);
+  //console.log(arguments);
+  const { document, rule } = context;
 
   const { owner: { rules } } = /** @type {Core.Rule} */ (rule);
-  // Check for API type
-  const apiType = targetValue.info?.["x-api-type"] || "standard";
+  console.log(`|${targetValue}| === |${options.value}|`)
+  console.log(options.rulesToAdjust)
 
-  if (apiType === "pygeoapi") {
+  // Check for API type
+  //const result = JSONPath({ path: options.condition.jsonPath, json: /** @type {string} */ (document.data) });
+  //const apiType = targetValue.info?.["x-api-type"] || "standard";
+
+  if (targetValue === options.value) {
     // Modify rule severity for legacy APIs
     console.log("Applying legacy overrides");
-    //console.log(context.rule.owner);
-    if (rules["should-have-info-x-contains-sensitive-data"]) {
-      console.log(rules["should-have-info-x-contains-sensitive-data"].definition)
-      rules["should-have-info-x-contains-sensitive-data"].severity = "info";
-      console.log(rules["should-have-info-x-contains-sensitive-data"].definition)
+
+    for (const [rule, severity] of Object.entries(options.rulesToAdjust)) {
+      if (rules[rule]) {
+        console.log("before:",rules[rule].definition.severity)
+        rules[rule].definition.severity = severity;
+        console.log("after:",rules[rule].definition.severity)
+        console.log(rules[rule].definition);
+      }
+      //rules["parameter-description"].severity = "info";
     }
-    //rules["parameter-description"].severity = "info";
   }
 
   return [];
 }
+
+export default createRulesetFunction({
+  input: null,
+  errorOnInvalidInput: true,
+  options: {
+    type: 'object',
+    description: 'Overrides certain rule severities based on custom JSONPath condition.',
+    additionalProperties: false,
+    properties: {
+      value: {
+        type: 'string',
+        description: 'The expected value to compare against.',
+      },
+      rulesToAdjust: {
+        type: 'object',
+        description: 'A mapping of rule IDs to their new severity levels.',
+        additionalProperties: {
+          type: 'string',
+          enum: ['error', 'warn', 'info', 'hint', 'off']
+        }
+      }
+    },
+    required: ['value', 'rulesToAdjust']
+  }
+}, overrideSeverity)
